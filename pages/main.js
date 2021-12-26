1b8b5998f6ab45eb7bdb2dfa11a1647a39d9845c
garam <rivery811@gmail.com>
2021-12-26 18:05:42 +0900

    axios

--- pages/main.js
+++ pages/main.js
@@ -3,12 +3,12 @@ import RoundButton from "../components/RoundButton";
 import LayoutHeader from "../components/LayoutHeader";
 import SquareInput from "../components/SquareInput";
 import PinkButton from "../components/PinkButton";
+import BasicBoard from "../components/BasicBoard";
 import Modal from "../components/Modal";
+
 const Main = () => (
   <>
-    <SquareInput label="ID" text="아이디를 입력해주세요"></SquareInput>
-    <PinkButton text="전체"></PinkButton>
-    <Modal title={"회원가입"}></Modal>
+    <BasicBoard></BasicBoard>
   </>
 );
 
